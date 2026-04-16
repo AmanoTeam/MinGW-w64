0ea9f4279b9b424faf51d2253d8b580e7f8c8a45
Kartatz <105828205+Kartatz@users.noreply.github.com>
2026-04-16 02:58:26 -0300

    Handle defined(MINGW) in the GCC wrapper

--- build.sh
+++ build.sh
@@ -51,9 +51,9 @@ declare -r linkflags='-Xlinker -s'
 declare -ra targets=(
 	# 'aarch64-w64-mingw32'
 	'x86_64-w64-mingw32-ucrt'
-	# 'x86_64-w64-mingw32-msvcrt'
-	# 'i686-w64-mingw32-ucrt'
-	# 'i686-w64-mingw32-msvcrt'
+	'x86_64-w64-mingw32-msvcrt'
+	'i686-w64-mingw32-ucrt'
+	'i686-w64-mingw32-msvcrt'
 )
 
 declare -r gcc_wrapper='/tmp/gcc-wrapper'
